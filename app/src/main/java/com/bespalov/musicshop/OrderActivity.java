eb1391c0e8e8b9745b123695209991b04ee5950d
Mikantare <abespalov33@gmail.com>
2020-10-02 19:41:43 +0300

    The End

--- app/src/main/java/com/bespalov/musicshop/OrderActivity.java
+++ app/src/main/java/com/bespalov/musicshop/OrderActivity.java
@@ -4,26 +4,48 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class OrderActivity extends AppCompatActivity {
 
     private TextView textViewOrder;
 
+    private String addresses = "abespalov33@gmail.ru";
+    private String subject = "Заказ";
+    private String order;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
+        this.setTitle("Your order");
         textViewOrder = findViewById(R.id.textViewOrder);
         Intent intent = getIntent();
         int quantity = intent.getIntExtra("quantity",0);
         double summ = intent.getDoubleExtra("summ",0);
         double price = summ/quantity;
-        textViewOrder.setText("Имя пользователя: " + intent.getStringExtra("name") + "." + "\nИнстумент: " + intent.getStringExtra("godsName") + "." + "\nКолличество : " + quantity + "\nЦена: " + price + "." + "\nСумма: " + summ);
-        this.setTitle("Your order");
+        order = "Имя пользователя: " + intent.getStringExtra("name") + "." +
+                "\nИнстумент: " + intent.getStringExtra("godsName") + "." +
+                "\nКолличество : " + quantity +
+                "\nЦена: " + price + "." +
+                "\nСумма: " + summ;
+        textViewOrder.setText(order);
     }
 
 
+    public void sendOrder(View view) {
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_EMAIL, addresses);
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        intent.putExtra(Intent.EXTRA_TEXT,order);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+
+    }
 }
